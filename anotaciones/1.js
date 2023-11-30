72f6c5fef37177f2066b2ccae680e1e4f45fa364
wshuu <franckyc2020@gmail.com>
2023-11-30 14:22:57 -0500

    mi segundo commit

--- anotaciones/1.js
+++ anotaciones/1.js
@@ -1,4 +1,45 @@
 //Aqui van las anotaciones del vidio de js
-const prueba = 'hola mundo';
+const prueba = 'hola mundo uwu < kz';
 alert(prueba);
  
+//Variable
+//Se declara una variable llamada 'recipiente' y se le asigna el valor 'papel'
+recipiente = 'papel';
+
+//Tipos de datos
+//Se declaran variables con diferentes tipos de datos: string, number y boolean
+string = 'cadena de texto';
+//Nota: Aunque se ha declarado como número, '9' es un string
+number = '9';
+boolean = false;
+
+//Tres formas de declarar variables: var, let, const
+//Se introducen las tres palabras clave para declarar variables
+//Const se inicializa una sola vez y no puede cambiar su valor
+const nombre = 'Franck';
+
+//Let y Var se pueden inicializar, declarar y modificar
+let num1 = 10;
+num1 = 11;
+
+var num2 = 9;
+num2 = 13;
+
+//Varias maneras de declarar y asignar valores a variables utilizando Var y Let
+//Primera forma: Declarar y luego asignar valor
+let num3;
+num3 = 15;
+
+//Segunda forma: Declarar e inicializar en una sola línea
+let num4 = 20;
+
+//Diferentes maneras de declarar múltiples variables
+//Primera forma: Declarar cada variable en líneas separadas
+var num5 = 4;
+var num6 = 5;
+
+//Segunda forma: Declarar varias variables en una sola línea
+var num7 = 6, num8 = 7;
+
+//Declarar una variable con valor nulo
+let num9 = null;
