396580923f565d3c38386e006ae7447371a4d7de
wshuu <franckyc2020@gmail.com>
2023-12-01 14:36:16 -0500

    mi tercer commit

---
+++ anotaciones/2.js
@@ -0,0 +1,129 @@
+//Aqui van las anotaciones del vidio de js #2
+
+//Nota: Para ver los diferentes resultados, quite los comentarios (//) de las líneas que contienen alert() o document.write()
+
+//Entrada de datos del usuario
+//Se utiliza la función `prompt` para solicitar al usuario que ingrese el nombre de su héroe favorito
+let nombre = prompt("Dime el nombre de tu héroe fav del dota2: ");
+
+//alert("(´• ω •`) " + nombre);
+
+
+//Operadores de asignacion
+//Se utilizan diferentes operadores de asignación para manipular variables
+
+let num1 = 10;
+
+num1 += 5 //num1 se incrementa en 5
+
+//document.write(num1);
+
+let num2 = 10;
+
+num2 -= 5 //num2 se reduce en 5
+
+//document.write(num2);
+
+let num3 = 10;
+
+num3 *= 5 //num3 se multiplica por 5
+
+//document.write(num3);
+
+let num4 = 10;
+
+num4 /= 5 //num4 se divide por 5
+
+//document.write(num4);
+
+let num5 = 10;
+
+num5 %= 6
+
+//document.write(num5);
+
+let num6 = 10;
+
+num1 **= 5 //num5 se divide por 6 y se asigna el residuo
+
+//document.write(num6);
+
+
+//Operadores Aritmeticos
+//Se utilizan para realizar operaciones matemáticas en variables
+
+num1 = 15;
+
+num2 = 5;
+
+resultado = num1 + num2; //Suma de num1 y num2
+//alert(resultado);
+
+num1 = 15;
+
+num2 = 5;
+
+num1--; //Decremento de num1 en 1
+
+resultado = num1;
+//alert(resultado);
+
+num1 = 15;
+
+num2 = 5;
+
+resultado = num1 / num2; //División de num1 entre num2
+//alert(resultado);
+
+num1 = 15;
+
+num2 = 5;
+
+resultado = num1 ** num2; //Elevación de num1 a la potencia de num2
+//alert(resultado);
+
+num1 = 15;
+
+num2 = 5;
+
+ num1++; //Incremento de num1 en 1
+
+resultado = num1;
+//alert(resultado);
+
+num1 = 15;
+
+num2 = 5;
+
+resultado = num1 & num2; //Operación de bits AND entre num1 y num2
+//alert(resultado);
+
+num1 = 15;
+
+num2 = 5;
+
+resultado = num1 - num2; //Resta de num2 de num1
+//alert(resultado);
+
+num1 = 15;
+
+num2 = 5;
+
+resultado = -num1; //Negación de num1
+//alert(resultado);
+
+//Concatenacion
+
+saludo = "¡Hola pedro!";
+pregunta = "¿Como estas?";
+
+frase = saludo + pregunta; //Concatenación de las cadenas
+
+//document.write(frase);
+
+num2 = "9";
+num3 = 0;
+
+frase = num2.concat(num3); // Concatenación de num7 y num8 como cadenas
+
+//document.write(frase);
