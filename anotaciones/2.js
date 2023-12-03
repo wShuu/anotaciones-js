3733e9c430f8d64ad07f02ff76105fec2f761b78
FlamesAlejandro <flameskazu@gmail.com>
2023-12-02 22:27:39 -0300

    code correction

--- anotaciones/2.js
+++ anotaciones/2.js
@@ -6,7 +6,7 @@
 //Se utiliza la función `prompt` para solicitar al usuario que ingrese el nombre de su héroe favorito
 let nombre = prompt("Dime el nombre de tu héroe fav del dota2: ");
 
-//alert("(´• ω •`) " + nombre);
+console.log("(´• ω •`) " + nombre);
 
 
 //Operadores de asignacion
@@ -16,37 +16,37 @@ let num1 = 10;
 
 num1 += 5 //num1 se incrementa en 5
 
-//document.write(num1);
+console.log(num1);
 
 let num2 = 10;
 
 num2 -= 5 //num2 se reduce en 5
 
-//document.write(num2);
+console.log(num2);
 
 let num3 = 10;
 
 num3 *= 5 //num3 se multiplica por 5
 
-//document.write(num3);
+console.log(num3);
 
 let num4 = 10;
 
 num4 /= 5 //num4 se divide por 5
 
-//document.write(num4);
+console.log(num4);
 
 let num5 = 10;
 
 num5 %= 6
 
-//document.write(num5);
+console.log(num5);
 
 let num6 = 10;
 
 num1 **= 5 //num5 se divide por 6 y se asigna el residuo
 
-//document.write(num6);
+console.log(num6);
 
 
 //Operadores Aritmeticos
@@ -57,7 +57,7 @@ num1 = 15;
 num2 = 5;
 
 resultado = num1 + num2; //Suma de num1 y num2
-//alert(resultado);
+console.log(resultado);
 
 num1 = 15;
 
@@ -66,51 +66,51 @@ num2 = 5;
 num1--; //Decremento de num1 en 1
 
 resultado = num1;
-//alert(resultado);
+console.log(resultado);
 
 num1 = 15;
 
 num2 = 5;
 
 resultado = num1 / num2; //División de num1 entre num2
-//alert(resultado);
+console.log(resultado);
 
 num1 = 15;
 
 num2 = 5;
 
 resultado = num1 ** num2; //Elevación de num1 a la potencia de num2
-//alert(resultado);
+console.log(resultado);
 
 num1 = 15;
 
 num2 = 5;
 
- num1++; //Incremento de num1 en 1
+num1++; //Incremento de num1 en 1
 
 resultado = num1;
-//alert(resultado);
+console.log(resultado);
 
 num1 = 15;
 
 num2 = 5;
 
 resultado = num1 & num2; //Operación de bits AND entre num1 y num2
-//alert(resultado);
+console.log(resultado);
 
 num1 = 15;
 
 num2 = 5;
 
 resultado = num1 - num2; //Resta de num2 de num1
-//alert(resultado);
+console.log(resultado);
 
 num1 = 15;
 
 num2 = 5;
 
 resultado = -num1; //Negación de num1
-//alert(resultado);
+console.log(resultado);
 
 //Concatenacion
 
@@ -119,11 +119,11 @@ pregunta = "¿Como estas?";
 
 frase = saludo + pregunta; //Concatenación de las cadenas
 
-//document.write(frase);
+console.log(frase);
 
 num2 = "9";
 num3 = 0;
 
 frase = num2.concat(num3); // Concatenación de num7 y num8 como cadenas
 
-//document.write(frase);
+console.log(frase);
